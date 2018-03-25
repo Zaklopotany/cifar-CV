b7698ff8fda4ab904b4188eadd6519994ad82733
zaklopotany <marry.jane.kelly@gmail.com>
2018-03-26 00:00:34 +0100

    repon

--- src/main/java/cifar/NearestNeighbor.java
+++ src/main/java/cifar/NearestNeighbor.java
@@ -1,9 +1,45 @@
 package cifar;
 
-public class NearestNeighbor {
-//compare algorithm
-	
-	
-	
+import java.util.List;
 
+public class NearestNeighbor {
+	// compare algorithm
+
+	public static void absolute(Image testImage, List<Image> compareImages, int imagesNumber) {
+		int compare = 0;
+
+		byte[] testImageDate = testImage.getImageData();
+
+		for (int i = 0; i < testImage.getImageData().length; i++) {
+			;
+		}
+	}
+
+	public class Neighbor implements Comparable<Neighbor> {
+		private Image img;
+		private int compareValue;
+
+		public Image getImg() {
+			return img;
+		}
+
+		public void setImg(Image img) {
+			this.img = img;
+		}
+
+		public int getCompareValue() {
+			return compareValue;
+		}
+
+		public void setCompareValue(int compareValue) {
+			this.compareValue = compareValue;
+		}
+
+		@Override
+		public int compareTo(Neighbor o) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+	}
 }
