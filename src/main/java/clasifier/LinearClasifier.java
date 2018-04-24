1332294e795b3acafc5598ab4476c956bddfb96e
zaklopotany <marry.jane.kelly@gmail.com>
2018-04-24 07:50:16 +0100

    cos

--- src/main/java/clasifier/LinearClasifier.java
+++ src/main/java/clasifier/LinearClasifier.java
@@ -1,26 +1,26 @@
 package clasifier;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Logger;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.cpu.nativecpu.NDArray;
 import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.ops.transforms.Transforms;
 
 import cifar.Image;
 import util.Pair;
 
 public class LinearClasifier {
 	private static final Random rand = new Random();
-	private Image[] data;
+	private List<Image> data;
 	private double step;
 	private INDArray weights;
-	private int classNum; // number of distinfguish categories
+	private int classNum; // number of distinguish categories
 	private double delta; // to calculate margin
-	private static final Logger logger = Logger.getLogger(LinearClasifier.class.getName());
-
-	private LinearClasifier(Image[] data, double step, int classNum, double delta) {
+	
+	private LinearClasifier(List<Image> data, double step, int classNum, double delta) {
 		super();
 		this.classNum = classNum;
 		this.step = step;
@@ -29,11 +29,11 @@ public class LinearClasifier {
 	}
 
 	// numClass - number of distinguish image categories
-	public static LinearClasifier getClasifierWithBiasTrick(Image[] data, double step, int classNum, double delta) {
+	public static LinearClasifier getClasifierWithBiasTrick(List<Image> data, double step, int classNum, double delta) {
 
 		LinearClasifier linearClass = new LinearClasifier(data, step, classNum, delta);
 		try {
-			linearClass.setInitialWeights(classNum, data[0].getImageData().length + 1, step);
+			linearClass.setInitialWeights(classNum, data.get(0).getImageData().length + 1, step);
 		} catch (Exception e) {
 			e.getMessage();
 			e.printStackTrace();
@@ -76,28 +76,42 @@ public class LinearClasifier {
 		INDArray label = Nd4j.create(size);
 
 		for (int i = 0; i < size; i++) {
-			Image tempImg = this.data[rand.nextInt(this.data.length)];
+			// get random image from data set
+			Image tempImg = this.data.get(rand.nextInt(this.data.size()));
 			data.put(i, Nd4j.create(LinearClasifier.convertToIntArray(tempImg.getImageData())));
 			label.putScalar(i, tempImg.getLabel());
 		}
-		Pair<INDArray,INDArray> tempPair = new Pair(data, label);
-		logger.info(" NEW BATCH ");
+		Pair<INDArray, INDArray> tempPair = new Pair(data, label);
+//		MainClassifier.logger.info(" NEW BATCH ");
 		return tempPair;
 	}
 
-	public INDArray calculateMarginSVM(Pair<INDArray, INDArray>  batchData, INDArray weights) {
-		
-//		INDArray margin = Nd4j.create(classNum, batchData.getKey().rows());
-		INDArray scores = batchData.getKey().mul(weights);
+	public INDArray calculateMarginSVM(Pair<INDArray, INDArray> batchData, INDArray weights) {
+
 		INDArray correctScores = Nd4j.create(batchData.getKey().rows());
-		
-		for(int i = 0; i<batchData.getKey().rows(); i++) {
-//			scores
-			batchData.getKey().getDouble(i, batchData.getValue().getInt(i));
-//			correctScores.put(i, b)
+		INDArray scores = batchData.getKey().mul(weights); // Sj - scores for each class
+//		MainClassifier.logger.info(" BATCH SCORES CALCULATED");
+
+		for (int i = 0; i < batchData.getKey().rows(); i++) {
+			// Syi - correct scores
+			correctScores.put(i, 0, batchData.getKey().getDouble(i, batchData.getValue().getInt(i)));
+		}
+//		MainClassifier.logger.info(" BATCH CORRECTSCORES CALCULATED");
+		// calculate margin and max(0,array)
+		INDArray margin = Transforms.max(scores.sub(correctScores).add(delta), 0);
+
+		// transform to binary array
+		margin = margin.gt(0);
+
+		for (int i = 0; i < correctScores.length(); i++) {
+			margin.put(i, correctScores.getInt(i), 0);
 		}
 
-		logger.info(" MARGIN CALCULATED");
+		INDArray sumArray = margin.sum(0);
+
+		INDArray ind = new NDArray();
+		return ind;
+		// delete Syi margin
 
 		// Wxi
 		// Wyi
---
+++ src/main/java/clasifier/MainClassifier.java
@@ -0,0 +1,27 @@
+package clasifier;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import cifar.Image;
+import cifar.ImageBinder;
+
+public class MainClassifier {
+	
+	public static final Logger logger = Logger.getLogger(MainClassifier.class.getName());
+	
+	public static void main(String[] args) throws InterruptedException {
+		logger.info("start");
+		
+		List<Image> images = ImageBinder.listOfAllImages();
+		
+		logger.info("images loaded, size  " + images.size());
+		
+		LinearClasifier linc; 
+				LinearClasifier.getClasifierWithBiasTrick(images, 0.00001, 10, 1);
+		
+		
+		Thread.sleep(1000);
+		
+	}
+}
